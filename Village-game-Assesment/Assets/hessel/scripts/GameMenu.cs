c9e714cb188096f9c4a668f0ea56158498182e8a
Omenio <hvr123uddel@gmail.com>
2024-03-05 15:44:46 +0100

    Ui veranderingen

---
+++ Village-game-Assesment/Assets/hessel/scripts/GameMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenu : MonoBehaviour
+{
+    public GameObject menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Menu"))
+        {
+            menu.SetActive(true);
+            
+        }
+
+
+    }
+}
---
+++ Village-game-Assesment/Assets/hessel/scripts/MainMenuUi.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUi : MonoBehaviour
+{
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+
+    public void Start()
+    {
+        
+    }
+
+}
