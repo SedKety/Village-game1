75533ebb6015194236ed689dd922c4788cc97f10
Omenio <hvr123uddel@gmail.com>
2024-04-04 13:23:56 +0200

    een paar sounds om te proberen.
     BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM  BOOM BOOM

--- Village-game-Assesment/Assets/CamScript.cs
+++ Village-game-Assesment/Assets/CamScript.cs
@@ -21,6 +21,7 @@ public class CamScript : MonoBehaviour
         if (canILook)
         {
             Cursor.lockState = CursorLockMode.Locked;
+
             transform.Rotate(Input.GetAxisRaw("Mouse X") * mouseSensitivity * Vector3.up);
             verticalLookRotation -= Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
             verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
